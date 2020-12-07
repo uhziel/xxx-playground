27d9a614a83ba3e493e23ad04b238e8f6c26e015
Lei Zhu <uhziel@gmail.com>
2020-12-07 12:55:29 +0800

    添加对 js array 的演示代码

---
+++ nodejs/4-array.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+// https://wangdoc.com/javascript/types/array.html
+// https://wangdoc.com/javascript/stdlib/array.html
+
+let fruits = ['apple', 'orange', 'banana'];
+
+console.log('fruits.length: ', fruits.length);
