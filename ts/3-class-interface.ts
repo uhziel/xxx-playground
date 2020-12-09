bc20bf37c7d2be171deff91f7976ab92a69e0180
uhziel <uhziel@gmail.com>
2020-12-09 09:02:03 +0800

    添加 interface、class 的试验

---
+++ ts/3-class-interface.ts
@@ -0,0 +1,31 @@
+#!/usr/bin/env ts-node-script
+
+interface Animal {
+  name(): string;
+}
+
+class Dog implements Animal {
+  foo: string;
+  constructor() {
+    this.foo = '123';
+  }
+  name() {
+    return 'Dog' + this.foo;
+  }
+}
+
+class Chicken implements Animal {
+  foo: string;
+  constructor() {
+    this.foo = '456';
+  }
+  name() {
+    return 'Chicken' + this.foo;
+  }
+}
+
+let a1: Animal = new Dog();
+let a2: Animal = new Chicken();
+
+console.log(a1.name());
+console.log(a2.name());
