1569010cbd4a68f6c7b39cd227d54475fc0b64cb
uhziel <uhziel@gmail.com>
2020-12-26 16:35:11 +0800

    添加个使用 mongodb driver 的示例

---
+++ mongodb-nodejs/index.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require("mongodb");
+// Replace the uri string with your MongoDB deployment's connection string.
+
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+  console.log('Please config your env MONGODB_URI first!');
+  return 0;
+}
+
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('sample_mflix');
+    const collection = database.collection('movies');
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await collection.findOne(query);
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
