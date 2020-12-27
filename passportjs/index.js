93a846479328b5b00a48d6dc702fca3ac219add0
uhziel <uhziel@gmail.com>
2020-12-27 11:02:23 +0800

    试用 express-session 和 connect-flash

--- passportjs/index.js
+++ passportjs/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
-const app = express();
+const session = require('express-session');
+const flash = require('connect-flash');
 
-const PORT = 3002;
+const app = express();
+app.use(session({
+  secret: 'wowuqu'
+}));
+app.use(flash());
+
+const PORT = 3000;
 
 app.get('/', function(req, res) {
-  res.send('helloworld');
+  req.flash('word', 'hi, flash!');
+  res.redirect('/flash');
+  
+});
+
+app.get('/flash', function(req, res) {
+  const word = req.flash('word');
+  res.send(`helloworld ${word}`);
 });
 
 app.listen(PORT, function() {
