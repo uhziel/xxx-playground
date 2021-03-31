470fdc5d2367aec70164ecaf757584e08882f0de
uhziel <uhziel@gmail.com>
2021-03-31 17:42:10 +0800

    再使用模版函数 compiledFunc 来生成数据

--- demo-pug/index.js
+++ demo-pug/index.js
@@ -3,3 +3,5 @@ const pug = require('pug');
 const compiledFunction = pug.compileFile('hello.pug');
 
 console.log(compiledFunction({name: 'zhulei'}));
+
+console.log(compiledFunction({name: 'world'}));
