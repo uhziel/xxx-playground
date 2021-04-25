9123ee09187055ed13280d1509608837e015323a
uhziel <uhziel@gmail.com>
2021-04-25 23:51:35 +0800

    学习 ejs 的基础用法

--- expressjs/demo-express-ejs/routes/index.js
+++ expressjs/demo-express-ejs/routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+    fruits: [
+      'Apple',
+      'Banana',
+      'Orange',
+    ]
+  });
 });
 
 module.exports = router;
