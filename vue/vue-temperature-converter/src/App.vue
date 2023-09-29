5eacf01b651851b5a76cbe9f576017519ab67230
uhziel <uhziel@gmail.com>
2023-09-29 23:07:05 +0800

    实现 vue-temperature-converter

---
+++ vue/vue-temperature-converter/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      c: 0,
+      f: 32,
+    }
+  },
+  methods: {
+    setC(c) {
+      this.c = c
+      this.f = c * (9 / 5) + 32
+    },
+    setF(f) {
+      this.f = f
+      this.c = (f - 32) * (5 / 9)
+    }
+  }
+}
+</script>
+
+<template>
+<input id="c" :value="c" @change="setC(+($event.target.value))"><label for="c">Celsius</label>=
+<input id="f" :value="f" @change="setF(+($event.target.value))"><label for="f">Fahrenheit</label>
+</template>
