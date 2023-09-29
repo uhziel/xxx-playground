240e17c0953ec04dfa6cb3a9b659652517345e5f
uhziel <uhziel@gmail.com>
2023-09-29 22:11:01 +0800

    实现 vue-simple-component

---
+++ vue/vue-simple-component/src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import TodoItem from './TodoItem.vue'
+
+export default {
+  components: {
+    TodoItem
+  },
+  data() {
+    return {
+      items: [
+        {id: 1, text: "Vegetables"},
+        {id: 2, text: "Cheese"},
+        {id: 3, text: "Whatever else humans are supposed to eat"},
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+<ol>
+  <TodoItem v-for="item in items" :key="item.id" :todo="item"></TodoItem>
+</ol>
+</template>
