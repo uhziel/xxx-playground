af56aa797e30ee263c0ead372d149d82304f47a8
uhziel <uhziel@gmail.com>
2023-09-29 17:54:54 +0800

    实现 vue-handling-input

---
+++ vue/vue-handling-input/src/App.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      message: "hello world",
+    }
+  },
+  methods: {
+    reverse() {
+      this.message = this.message.split('').reverse().join('')
+    },
+    tip() {
+      alert("you click a link with e.preventDefault()!")
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <button @click="reverse">Reverse Message</button>
+  <br>
+  <button @click="message += '!'">Append "!"</button>
+  <br>
+  <a href="http://example.com" @click.prevent="tip">A link with e.preventDefault()</a>
+  <br>
+</template>
