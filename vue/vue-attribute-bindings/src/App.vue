61c73edc51b9d324ad6eb67a674e94062e8ff796
uhziel <uhziel@gmail.com>
2023-09-29 18:09:10 +0800

    实现 vue-attribute-bindings

---
+++ vue/vue-attribute-bindings/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "hello world",
+      isRed: true,
+      color: 'green',
+    }
+  },
+  methods: {
+    toggleRed() {
+      this.isRed = !this.isRed
+    },
+    toggleColor() {
+      this.color = (this.color === 'green' ? 'blue' : 'green')
+    }
+  }
+}
+</script>
+
+<template>
+  <p :title="message">Hover your mouse over me for a few seconds to see my dynamically bound title!</p>
+  <p :class="{ red: isRed }" @click="toggleRed">This should be red... but click me to toggle it.</p>
+  <p :style="{ color }" @click="toggleColor">This should be green, and should toggle between green and blue on click.</p>
+</template>
+
+<style>
+.red {
+  color: red;
+}
+</style>
