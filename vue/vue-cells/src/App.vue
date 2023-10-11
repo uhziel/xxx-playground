1a581a4b746b1492a57fe9342f02ec1a0573d026
uhziel <uhziel@gmail.com>
2023-10-11 21:50:43 +0800

    实现 vue-cells

---
+++ vue/vue-cells/src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import {cells} from './store.js'
+import Cell from './Cell.vue'
+
+export default {
+    components: {
+        Cell,
+    },
+    data() {
+        return {
+            cols: cells.map((_, i) => String.fromCharCode(65 + i)),
+            cells,
+        }
+    },
+}
+</script>
+
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th></th>
+                <th v-for="col in cols">{{ col }}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="y in cells[0].length">
+                <th>{{ y - 1 }}</th>
+                <td v-for="(col, x) in cols">
+                    <Cell :r="y - 1" :c="x"></Cell>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
---
+++ vue/vue-cells/src/Cell.vue
@@ -0,0 +1,41 @@
+<script>
+import {cells, evalCell} from './store.js'
+
+export default{
+    props: {
+        c: Number,
+        r: Number,
+    },
+    data() {
+        return {
+            editing: false,
+            cells,
+        }
+    },
+    methods: {
+        update(e) {
+            this.editing = false
+            cells[this.c][this.r] = e.target.value.trim()
+        },
+        evalCell
+    }
+}
+</script>
+
+<template>
+    <div @click="editing = true">
+        <input
+            v-if="editing"
+            :value="cells[c][r]"
+            @change="update"
+            @blur="update"
+            @vue:mounted="({ el }) => el.focus()">
+        <span v-else>{{ evalCell(cells[c][r]) }}</span>
+    </div>
+</template>
+
+<style scoped>
+div {
+    height: 1.5em;
+}
+</style>
